ccb7dcdfec7d8b3b45b54204fa2cdc5c14e617de
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-09 00:47:13 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Takes no parameters
+*Description: Usage of printf to print to stdio
+*Return: zero if successfully executed
+*/
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
