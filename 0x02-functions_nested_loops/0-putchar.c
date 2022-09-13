44265151f248b588e3ef5a44c30b905874fdc94a
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-13 11:41:09 +0300

    Print _putchar to the stdio

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main -  This is the entry function
+ *Description: This function should print _putchar followed by a new line
+ *Return: The main fuction returns 0
+ */
+
+int main(void)
+{
+char name[10] = "_putchar";
+int length = 0, char_pos;
+length = strlen(name);
+for (char_pos = 0; char_pos < length; char_pos++)
+{
+putchar(name[char_pos]);
+}
+putchar('\n');
+return (0);
+}
