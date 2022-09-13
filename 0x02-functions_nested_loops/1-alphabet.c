e61b890a7331aa95f4e9851a37785dc0a64e7b06
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-13 12:28:51 +0300

    added a function prototype to print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- *main - Entry function,all c -program has this
+ *print_alphabet - This function prints the alphabets
  *
  *Return: always 0
  */
 
 void print_alphabet(void)
 {
-char ch ;
-for (ch = 'a' ; ch <='z'; ch++)
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
