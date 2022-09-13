c52704ba81cdb4c988bdceb457dfaa6ce8955edb
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-13 21:42:50 +0300

    Added a function to check lower case characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+
+/**
+ * _islower - Checks if character is lower case 
+ *Return:return 1 if character is lower case else  return 0
+ */
+int _islower(int c)
+{
+if(islower(c))
+{
+  return (1);
+}
+else
+{
+  return (0);
+} 
+}
