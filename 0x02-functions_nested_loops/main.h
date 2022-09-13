9bc9ded608fa1738acf97a5f2e57f190c7c1abb2
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-13 22:23:57 +0300

    .

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
 /**
- *_putchar function is used tp print characters
- *Return: The main fuction returns 0
- */
+*_putchar - function call to print characters
+*Description: Prototype functions
+*@c : The parameter to be checked
+*Return: The main fuction returns 0
+*/
 int _putchar (char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+#endif
