e6076fa7f2ba3be6d5198c5b2f8800f77e5d0eae
Edison Gachomo Ngunjiri <44634585+ENG-EDISON@users.noreply.github.com>
2022-09-16 00:35:39 +0300

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,11 +3,11 @@
 
 /**
  * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
+ * left in the year.
  * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
+ * @day: day of month parameter
+ * @year: year parameter
+ * Return: return nothing
  */
 
 void print_remaining_days(int month, int day, int year)
