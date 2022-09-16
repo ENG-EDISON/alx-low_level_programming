c600a4eb107ec2dcb311d6401010e06b54a48751
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-16 11:07:19 -0700

    Multipliccation function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*mul - Function that returns the result of multiplication
+*of a and b
+*@a: First parameter
+*@b: Second parameter
+*Return: @a multiplied by @b
+*/
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
