50b4930bb3ea022e86a2439812781e59b7b45b56
Edison Gachomo Ngunjiri <44634585+ENG-EDISON@users.noreply.github.com>
2022-09-16 23:52:43 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- *print_diagonal - printd diagonal lines of '\'
- *@n:Number of times to print the backslash
+ * print_diagonal - function that draws a diagonal line using '\'.
+ * @n: numer of times
+ * 
  */
+
 void print_diagonal(int n)
 {
-if (n > 0)
-{
-for (int i = 0; i < n; i++)
-{
-for (int k = n; k <= n + i; k++)
-{
-_putchar(' ');
-}
-_putchar(92);
-_putchar('\n');
-}
-}
-_putchar('\n');
+	int i = 0;
+	int j = 0;
+
+	while (i < n)
+	{
+		while (j <= i)
+		{
+			if (j != i)
+				_putchar(' ');
+			else
+				_putchar(92);
+			j += 1;
+		}
+		_putchar('\n');
+		i += 1;
+		j = 0;
+	}
+	if (n <= 0)
+		_putchar('\n');
 }
