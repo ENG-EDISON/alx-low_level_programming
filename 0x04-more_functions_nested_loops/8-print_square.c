0bbbcee7466abfd8dc92725f9e6a69fe0344f5dc
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-16 23:36:24 -0700

    Square of #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,28 +1,25 @@
 #include "main.h"
-
 /**
  * print_square - function that prints a square using #, followed by a new line
  * @size: size of square
  * Return: nothing.
  */
-
 void print_square(int size)
 {
-	int col, row;
-
-	if (size > 0)
-	{
-		for (row = 0; row < size; row++)
-		{
-			for (col = 0; col < size; col++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
-		}
-	}
-	else
-  {
-		_putchar('\n');
-  }
+int col, row;
+if (size > 0)
+{
+for (row = 0; row < size; row++)
+{
+for (col = 0; col < size; col++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
