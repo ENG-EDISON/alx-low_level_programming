15b9c565ed2c84c1f088fdc296e8412768f08fd3
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-20 01:04:09 -0700

    Function to rest integer variables to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- *reset_to_98 - Function to reset a parameter @n to 98
- *@n: The parameter variable to be reset to 98.
- *example: int number=10; reset_to_98(&number);
+*reset_to_98 - Function to reset integer variables to 98
+*@n: The parameter variable to be reset to 98.
+*example: int number=10;
+*reset_to_98(&number);
  *
  */
 void reset_to_98(int *n)
 {
-*n=98;
+*n = 98;
 }
