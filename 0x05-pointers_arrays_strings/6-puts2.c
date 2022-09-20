42aed8e3a21ebb74959ff16e7d0c6b330f25f95d
Edison Gachomo Ngunjiri <44634585+ENG-EDISON@users.noreply.github.com>
2022-09-20 17:27:11 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,6 @@
 
 void puts2(char *str)
 {
-<<<<<<< HEAD
 int n = 0, k = 0;
 while (str[k] != '\0')
 {
@@ -18,17 +17,4 @@ n = n + 2;
 k++;
 }
 _putchar('\n');
-=======
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-		{
-			_putchar(*(str + i));
-		}
-		i += 1;
-	}
-	_putchar('\n');
->>>>>>> d0ff4cd60123bb8fbb8008f3f1f330cf38ba86c4
 }
