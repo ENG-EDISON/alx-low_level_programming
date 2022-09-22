e9b4157f072acc4e2932f03a0b8cdcbe27faaa01
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-22 10:30:46 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,18 +5,18 @@
  *@s2: String 2
  *Return: 0 if equal, >0 if s1>s2, <0 if s1<s2
  */
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 int i;
-for (i = 0; s[i] == t[i]; i++)
+for (i = 0; s1[i] == s2[i]; i++)
 {
-if (s[i] == '\0')
+if (s1[i] == '\0')
 {
 return (0);
 }
 else
 {
-return (s[i] - t[i]);
+return (s1[i] - s2[i]);
 }
 }
 }
