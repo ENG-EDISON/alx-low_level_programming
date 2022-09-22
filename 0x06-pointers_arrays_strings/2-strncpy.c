d26b48b2b5d262d729c7f3c82a86caab5da1ecf9
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-22 10:05:35 -0700

    copy n characters to a buffer

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,17 +8,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-unsigned int i = 0;
-int j = 0;
-while (*(src + j) != '\0')
+int i = 0;
+while (i < n && *(src + i) != '\0')
 {
-if (j < n)
-{
-*(dest + i) = *(src + j);
+*(dest + i) = *(src + i);
 i++;
 }
-j++;
-}
-*(dest + i) = '\0';
+dest[i++] = '\0';
 return (dest);
 }
