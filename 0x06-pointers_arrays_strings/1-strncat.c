6aacdd0c1544e683106b82ceca0897e6053dba4a
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-22 08:31:13 -0700

    join string upto n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strncat - Function to join two strings
+ *@dest: The final destination after joining
+ *@src:The string to append to dest
+ *@n: The number of charactres to append
+ *Return: A pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (dest[i++] = src[j++] != '\0')
+{
+if (j < n)
+{
+break;
+}
+}
+return (dest);
+}
