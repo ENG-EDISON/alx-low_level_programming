0a3448936fa4861b91f5fe8896bbd1de23548cbd
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-22 09:44:03 -0700

    join two strings to n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, j = 0;
-while (dest[i] != '\0')
+unsigned int i = 0;
+int j = 0;
+while (*(dest + i) != '\0')
 {
 i++;
 }
-while (j < n)
+while (*(src + j) != '\0')
 {
-dest[i++] = src[j++];
+if (j < n)
+{
+*(dest + i) = *(src + j);
+i++;
 }
+j++;
 }
 return (dest);
 }
