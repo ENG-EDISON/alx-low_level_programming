596de2876d298b9ddbf66b192d535d362247c357
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-22 09:58:00 -0700

    copy n characters to a buffer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strncpy - Function to copy strings from src to dest
+ *@dest: The final destination after joining
+ *@src:The string to append to dest
+ *@n: The number of charactres to copy
+ *Return: A pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+unsigned int i = 0;
+int j = 0;
+while (*(src + j) != '\0')
+{
+if (j < n)
+{
+*(dest+i) = *(src+j);
+i++;
+}
+j++;
+}
+return (dest);
+}
