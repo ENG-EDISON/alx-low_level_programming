39bf587d9a144950a0fde3219a0cc6234abbbc88
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-09-26 10:07:07 -0700

    function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_memset - is a function to set a a string
+*@s:the string to copy to
+*@c:the character to be copied to
+*@n:The number of characters to be copied to.
+*/
+void *_memset(void *s, int c, size_t n)
+{
+unsigned int index = 0;
+unsigned char *dest = s, hold = c;
+for (index = 0; index < n; index++)
+{
+dest[index] = hold;
+}
+return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning pointer array strings
