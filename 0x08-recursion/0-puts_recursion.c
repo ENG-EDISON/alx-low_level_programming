65d67ddbfdf667cd679b5ecab15c0a4ba37f45fc
Edison Gachomo Ngunjiri <44634585+ENG-EDISON@users.noreply.github.com>
2022-09-28 19:57:38 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-if (*(s) != '\0')
+if(*(s) == '\0')
 {
-_putchar(*(s++));
-_puts_recursion(s);
-_putchar('\0');
+putchar('\0');
+return;
 }
+putchar(*s);
+_puts_recursion(s+1);
 }
