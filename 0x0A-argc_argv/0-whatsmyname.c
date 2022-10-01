37a43664ad5e4d84b1a3ffbcfa56020fa59eb612
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-10-01 01:50:02 -0700

    program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Entry Function to get the code name
+ *@argc:The number of command lines
+ *@argv:The NULL pointer
+ *Return: 0 is successful
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+printf("\n");
+return (0);
+}
