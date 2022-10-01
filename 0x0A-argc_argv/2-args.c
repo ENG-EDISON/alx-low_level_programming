640ceff730657b8117c1963de029f3264cc9e0ac
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-10-01 02:34:04 -0700

    program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Entry function to print the commans
+ *@argc:The number of commands
+ *@argv:The NULL pointer to command
+ *Return:0 if successful
+ */
+int main(int argc, char *argv[])
+{
+int i = 0;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+printf("\n");
+return (0);
+}
