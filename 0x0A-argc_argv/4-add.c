c19e53da8b686937c269d98acb124b080fa43674
Edison Gachomo Ngunjiri <44634585+ENG-EDISON@users.noreply.github.com>
2022-10-01 22:46:16 +0300

    number of coins

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,62 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- *main - Entry function to print the multiplication
- *of two numbers in the command line
- *@argc:The number of commands
- *@argv:The NULL pointer to command
- *Return:0 if successful
+ * main - adds positive numbers and prints the result
+ * @argc: number of arguments passed
+ * @argv: pointer to array of arguments passed
+ *
+ * Return: 0 on success
  */
+
 int main(int argc, char *argv[])
 {
-int balance = 0;
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-else
-{
-balance = atoi(argv[1]);
-}
-if (balance < 0)
-{
-printf("0\n");
-}
-else
-{
-int balance = atoi(argv[1]);
-get_coins_change(balance);
-}
-return (0);
-}
-/**
- *get_coins_change - Get the minimum number of coins
- *@change:The balance amount
- *Return: The number of coins
- *
- */
-void get_coins_change(int change)
-{
-const int coins[5] = {25, 10, 5, 2, 1};
-int count = 0;
-int Hold = change;
-int number_coins = 0;
-int remainder = 0;
-for (count = 0; count < 5; count++)
-{
-remainder = Hold / coins[count];
-if (remainder >= 1)
-{
-number_coins += remainder;
-Hold = Hold - (coins[count] * remainder);
-remainder = 0;
-}
-if (Hold == 0)
-{
-printf("%d\n", number_coins);
-break;
-}
-}
+	int res = 0;
+	int num = 0;
+	char *ptr = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	++argv;
+	--argc;
+	while (argc--)
+	{
+		num = (int)strtol(*argv, &ptr, 10);
+		res = res + num;
+
+		if (*ptr != 0 || num < 0 || res < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		++argv;
+	}
+	printf("%d\n", res);
+
+	return (0);
 }
