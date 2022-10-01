6e990af56576833e187faa899aec2df7ef101cd4
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-10-01 12:38:16 -0700

    number of coins

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,19 +10,54 @@
  */
 int main(int argc, char *argv[])
 {
-int num, digit, sum = 0;
-for (num = 1; num < argc; num++)
-{
-for (digit = 0; argv[num][digit]; digit++)
-{
-if (argv[num][digit] < '0' || argv[num][digit] > '9')
+int balance = 0;
+if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
+else
+{
+balance = atoi(argv[1]);
 }
-sum += atoi(argv[num]);
+if (balance < 0)
+{
+printf("0\n");
 }
-printf("%d\n", sum);
+else
+{
+int balance = atoi(argv[1]);
+get_coins_change(balance);
+}
 return (0);
+}
+/**
+ *get_coins_change - Get the minimum number of coins
+ *@change:The balance amount
+ *Return: The number of coins
+ *
+ */
+int get_coins_change(int change)
+{
+const int coins[5] = {25, 10, 5, 2, 1};
+int count = 0;
+int Hold = change;
+int number_coins = 0;
+int remainder = 0;
+for (count = 0; count < 5; count++)
+{
+remainder = Hold / coins[count];
+if (remainder >= 1)
+{
+number_coins += remainder;
+Hold = Hold - (coins[count] * remainder);
+remainder = 0;
+}
+if (Hold == 0)
+{
+printf("%d\n", number_coins);
+break;
+}
+}
+return (number_coins);
 }
